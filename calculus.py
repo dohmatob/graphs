580e6b5219c690c0a0ca033293a8402247c1cd8d
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-05-03 13:32:08 +0200

    new modules

---
+++ calculus.py
@@ -0,0 +1,413 @@
+"""
+:Module: calculus
+:Author: dohmatob elvis dopgima <elvis[dot]dohmatob[at]inria[dot]fr>
+:Synopsis: Routine functions for tricky business like Markov dynamics,
+diffusion kernels, modularity computation, etc.
+
+"""
+
+import numpy as np
+import scipy.sparse
+import scipy.linalg
+import scipy.stats
+import networkx as nx
+
+
+def wikipedia_example_graph():
+    import itertools
+
+    G = nx.Graph()
+    for a in xrange(0, 9, 3):
+        nodes = xrange(a, a + 3)
+        G.add_edges_from(itertools.combinations(nodes, 2))
+        G.add_edge(a, 9)
+
+    return G
+
+
+def rosval_mapequation_example_graph_1():
+    G = nx.DiGraph()
+    for a in xrange(0, 16, 4):
+        nodes = xrange(a, a + 4)
+        H = nx.DiGraph()
+        H.add_nodes_from(nodes)
+        H.add_edges_from([(x, ((x + 1) % 4 + a)) for x in nodes])
+        G = nx.union(G, H)
+
+    G.add_edges_from([(1, 5),
+                      (2, 4),
+                      (6, 10),
+                      (7, 9),
+                      (8, 14),
+                      (11, 15),
+                      (13, 3),
+                      (12, 0)
+                      ])
+
+    return G
+
+
+def compute_adjacency_matrix(G):
+    """Computes the adjacency matrix of a graph or di-graph.
+
+    Returns
+    -------
+    2D array
+
+    """
+
+    iG = nx.convert_node_labels_to_integers(G)
+    adj_list = iG.adjacency_list()
+    n_nodes = len(iG.nodes())
+
+    adj_mat = np.zeros((n_nodes, n_nodes))
+    for x in xrange(n_nodes):
+        adj_mat[x, adj_list[x]] = 1
+
+    return adj_mat
+
+
+def compute_modularity_matrix_from_adjacency_matrix(A):
+    in_degree = np.sum(A, axis=0)
+    out_degree = np.sum(A, axis=1)
+    m = np.sum(A)
+
+    return A - 1. * np.outer(out_degree, in_degree) / m
+
+
+# def compute_modularity_matrix(G):
+#     return compute_modularity_matrix_from_adjacency_matrix(
+#         compute_adjacency_matrix(G))
+
+
+def compute_steady_state_pi(adj_mat):
+    """Computes the steady-stead distribution of a random walk on an undirected
+    graph, Given its adjacency matrix.
+
+    Parameters
+    ----------
+    adj_mat: 2D array
+        The adjacency matrix of the underlying graph
+
+    Returns
+    -------
+    1D array
+
+    """
+
+    return 1. * np.sum(adj_mat, axis=0) / np.sum(adj_mat)  # d_j / 2|E|
+
+
+def compute_transition_rates(adj_mat):
+    """Computes the transition matrix (of flow rates) for the embedded Markov
+    chain associeted with a random walk on a graph. The computed matrix embeds
+    transition rates, not necessary
+    probs!!!
+
+    Parameters
+    ----------
+    adj_mat: 2D array
+        The adjacency matrix of the underlying graph
+
+    Returns
+    -------
+    2D array
+
+    """
+
+    # compute the node degrees
+    degrees = np.sum(adj_mat, axis=0)
+    degrees[degrees == 0] = adj_mat.shape[0]
+
+    # compute D = diag(1 / degree_i), for each node i
+    D = scipy.sparse.csr_matrix(np.diag(1. / degrees))
+
+    # compute the transition matrix M = A\D
+    M = D.dot(adj_mat)
+
+    return M
+
+
+def compute_laplacian(adj_mat, normalized=False):
+    # compute the node degrees
+    degrees = np.sum(adj_mat, axis=0)
+    degrees[degrees == 0] = adj_mat.shape[0]
+
+    # compute D = diag(1 / degree_i), for each node i
+    D = np.diag(degrees)
+
+    # compute laplacian
+    L = D - adj_mat
+
+    if normalized:
+        Z = np.diag(np.sqrt(1. / np.diag(D)))
+        L = np.dot(Z, np.dot(L, Z))
+
+    return L
+
+
+def compute_infinitesimal_generator(adj_mat):
+    n = len(adj_mat)
+
+    degrees = np.sum(adj_mat, axis=0) * 1.
+    Dinv = scipy.sparse.csr_matrix(np.diag(1. / degrees))
+
+    return Dinv.dot(adj_mat) - np.eye(n)
+
+
+def compute_CTMC_flow(adj_mat, time):
+    """Computes the CTMC (Continous-Time Markov Chain) flow associated with
+    a given graph, over a set of time instants
+
+    Parameters
+    ----------
+    adj_mat: 2D array-like
+        the adjacency matrix of the underlying graph
+
+    time: 1D array-like
+        time instants we want to compute CTMC flow for
+
+    Returns
+    -------
+    3D array of shape (n_nodes, n_nodes, len(time)), corresponding to the
+    flow (one per time point) orderly packed together into an film
+
+    """
+
+    n = len(adj_mat)
+    T = len(time)
+
+    # compute infinitesimal generator
+    degrees = np.sum(adj_mat, axis=0) * 1.
+    D = scipy.sparse.csr_matrix(np.diag(degrees))
+    generator_mat = compute_infinitesimal_generator(adj_mat)
+
+    flow = np.zeros((n, n, T))
+    for j in xrange(T):
+        flow[:, :, j] = D.dot(scipy.linalg.expm(time[j] * generator_mat))
+
+    return flow
+
+
+def sparse_expm_dominant_eigenmodes(A, k=6, t=1.):
+    [d, U] = scipy.sparse.linalg.eigs(A, k=k, which='LR')
+    n = len(A)
+    expo = np.zeros((n, n))
+    for j in xrange(U.shape[1]):
+        expo += np.exp(t * d[j]) * np.dot(U[:, j:j + 1], U[:, j:j + 1].T)
+
+    return expo
+
+
+def compute_CTMC_flow_in_logspace(adj_mat, time_resolution=.1,
+                                  n_timepoints=10):
+    """Computes the CMTC (Continuous-Time Markov Chain) in logspace (base 2)
+    time, associated witha a given graph.
+
+    Parameters
+    ----------
+    adj_mat: 2D array-like
+        the adjacency matrix of the underlying graph
+
+    time_resolution: float (optional, default .1)
+        we'll only consider mutliples/bins of this time
+
+    n_timepoints: int (optional, default 10)
+        number of time points
+
+    Returns
+    -------
+    3D array or shape (n_nodes, n_nodes, n_timepoints), corresponding to the
+    flow (one per time point) orderly packed together into an film
+
+    """
+
+    n = len(adj_mat)
+
+    # compute infinitesimal generator
+    degrees = np.sum(adj_mat, axis=0) * 1.
+    D = scipy.sparse.csr_matrix(np.diag(degrees))
+
+    generator_mat = compute_infinitesimal_generator(adj_mat)
+
+    # compute 2nd-order approximation to e^(time_resolution * generator_mat)
+    tmp = time_resolution * generator_mat
+    U = np.eye(n) + tmp + np.linalg.matrix_power(
+        tmp, 2) / 2.
+
+    print "Bridge passed .."
+    # Techinique: U^2k = (U^k)^2
+    for j in xrange(n_timepoints):
+        # compute flow matrix at time t = time_resolution * 2 ^j
+        V = D.dot(U)
+
+        # update U
+        U = np.linalg.matrix_power(U, 2)
+
+        # yield compouted flow matrix
+        yield V
+
+
+def compute_modularity_from_modularity_matrix(
+    B, partition,
+    n_edges=None,
+    directed=False):
+    m = len(partition)
+    n_nodes = B.shape[0]
+
+    if isinstance(partition, np.ndarray):
+        S = partition.T
+    else:
+        # S[c, n] is 1 if node n is in community c; 0 otherwise
+        S = np.zeros((m, n_nodes))
+        for j, c in zip(xrange(m), partition):
+            S[j, partition[c]] = 1
+
+    Q = np.trace(np.dot(S, np.dot(B, S.T)))
+    if n_edges:
+        Q /= n_edges
+
+    return Q
+
+
+def compute_modularity_from_adjacency_matrix(adj_mat, partition,
+                                             directed=False):
+    m = len(partition)
+    n_nodes = adj_mat.shape[0]
+    n_edges = np.sum(adj_mat)
+    if directed:
+        n_edges *= 2
+
+    if isinstance(partition, np.ndarray):
+        S = partition.T
+    else:
+        # S[c, n] is 1 if node n is in community c; 0 otherwise
+        S = np.zeros((m, n_nodes))
+        for j, c in zip(xrange(m), partition):
+            S[j, partition[c]] = 1
+
+    B = compute_modularity_matrix_from_adjacency_matrix(adj_mat)
+
+    return np.trace(np.dot(S, np.dot(B, S.T))) / n_edges
+
+
+def compute_modularity(G, partition):
+    # total edge count (undirected edges double countedd)
+    n_edges = len(G.edges())
+    if not G.is_directed():
+        n_edges *= 2
+
+    adj_mat = compute_adjacency_matrix(G)
+
+    return compute_modularity_from_adjacency_matrix(
+        adj_mat,
+        partition
+        )
+
+
+def improved_2_split(B_g, s_g):
+    n_g = len(s_g)
+    s_g = np.array(s_g)
+    unmoved = list(xrange(n_g))
+
+    for i in xrange(n_g):
+        score = np.zeros(len(unmoved))
+        Q_0 = 0.5 * np.dot(s_g.T, np.dot(B_g, s_g))
+        for k in xrange(len(unmoved)):
+            s_g[unmoved[k]] *= -1
+            score[k] = 0.5 * np.dot(s_g.T, np.dot(B_g, s_g)) - Q_0
+            s_g[unmoved[k]] *= -1
+
+        j_ = np.argmax(score)
+        if score[j_] > 0:
+            s_g[j_] *= -1
+            unmoved.remove(unmoved[j_])
+        else:
+            break
+
+    return s_g
+
+
+def compute_B_g(B, g):
+    B_g = B[g, :][:, g]
+    B_g -= np.diag(B_g.sum(axis=0))
+
+    return B_g
+
+
+def split_module(B, g):
+    """Tries to split split a module g.
+
+    Parameters
+    ----------
+    B: numpy 2D array
+        modularity matrix of underlying graph/network
+    g: array-like
+        module to be splitted
+
+
+    Returns
+    -------
+    A piece of g (or g, if can't split)
+
+    """
+
+    # slice B with g x g
+    n_g = len(g)
+    B_g = compute_B_g(B, g)
+
+    # compute the spectrum (sorted by ascending eigen values) of B_g
+    eig_vals, eig_vecs = np.linalg.eig(B_g)
+    sort_perm = np.argsort(eig_vals)
+    eig_vals = eig_vals[sort_perm]
+    eig_vecs = eig_vecs[sort_perm]
+
+    # try to split g
+    if eig_vals[-1] > 0:
+        s_g = np.ones(n_g)
+        s_g[eig_vecs[-1] < 0] = -1
+        s_g = improved_2_split(B_g, s_g)
+        dQ = 0.5 * np.dot(s_g.T, np.dot(B_g, s_g))
+        if dQ > 0:
+            g_1 = np.array(g)[np.nonzero(s_g > 0)[0]]
+
+            # return chopped-off component of g
+            return g_1
+
+    # g is indivisible
+    return g
+
+
+# def newman_split(G):
+#     B = compute_modularity_matrix(G)
+#     P = {"divisible": [G.nodes()], "indivisible": []}
+#     k = 0
+#     while P["divisible"]:
+#         k += 1
+#         g = P["divisible"][0]
+#         g_1 = split_module(B, g)
+#         if len(g_1) in [0, len(g)]:
+#             P["divisible"].remove(g)
+#             P["indivisible"].append(g)
+#         else:
+#             g_2 = [x for x in g if not x in g_1]
+#             P["divisible"].remove(g)
+#             for g in [g_1, g_2]:
+#                 if len(g_1) == 1:
+#                     P["indivisible"].append(g)
+#                 else:
+#                     P["divisible"].append(g)
+
+#     return P
+
+# # A = np.zeros((5, 5))
+# # A[0,1] = A[2,3] = A[2,4] = A[3,4] = A[4,3] = A[4,2] = A[3,2] = A[1,0] = 1
+# # B = compute_modularity_matrix_from_adjacency_matrix(A)
+# # split_module(B, [0, 3, 4])
+# H = rosval_mapequation_example_graph_1()
+# B = compute_modularity_matrix(H)
+# print newman_split(H)
+# # g = [0, 1, 2, 9]
+# # print improved_2_split(B, [-1, 1, 1, -1, -1, -1, -1, -1, -1, -1])
+# # print compute_modularity(H, [[0, 1, 2], xrange(3, 10)])
+# # print compute_modularity(H, [[3, 4, 5, 6, 7, 8], [0, 1, 2, 9]])
---
+++ markov_zoomer.py
@@ -0,0 +1,281 @@
+"""
+:Module: markov_zoom
+:Synopsis: Michael Schauss' Markov time Sweeping of the Map-Equation
+:Author: dohmatob elvis dopgima
+
+"""
+
+import os
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
+import commands
+import numpy as np
+import random
+
+import parsers
+import calculus
+
+
+def do_markov_zoom(adj_mat, filename='/tmp/zoom', time=None, n_attempts=10,
+                   is_connected=False,
+                   do_plots=True):
+    """Runs the Michael Schaub et al. Markov Zooming algorithm.
+
+    Parameters
+    ----------
+    adj_mat: np.ndarray (2 x 2)
+        adjacency matrix of underlying network/graph
+
+    filename: string
+        prefix for .net, .clu, .tree, etc., output files
+
+    time: iterable of object of floats (list, array, etc., optional;
+    default None)
+        series of Markov time gaps (resolutions) we're interested in
+
+    n_attempts: int (optional, default 10)
+    number of attemps to split the network done by the map-equation back-end
+
+    do_plots: boolean (optional, default True)
+        if true, plots of the clustering will be generated
+
+    Returns
+    -------
+    clustering_mat: 2 x 2 np.ndarray of shape of ints (#nodes, #time points)
+        clustering matrix for the network; each column contains the cluster
+        membership of nodes to modules, in/at each markov time gap (resolution)
+
+    time: np.array
+        Markov time gaps/resolutions at which zooming has been done
+
+    modularity: np.array
+        array of modularities of the partitionings described in clustering_mat
+
+    """
+
+    # sanity business
+    filename = os.path.join("/tmp",
+                            os.path.basename(filename).split(".")[0])
+
+    G = nx.convert_node_labels_to_integers(nx.Graph(adj_mat))
+
+    G_nodes = G.nodes()
+    if not isinstance(adj_mat, np.ndarray):
+        adj_mat = calculus.compute_adjacency_matrix(G)
+
+    nx.write_graphml(G, filename + '.graphml')
+
+    # compute connected components
+    if not is_connected:
+        print "[+] Computing connected components of network .."
+        connected_components = nx.connected_component_subgraphs(G)
+        print "[+] Done (%i connected components)" % len(connected_components)
+    else:
+        connected_components = [G]
+
+    # prepare timepoints
+    logspace = False
+    if time is None:
+        logspace = True
+        n_timepoints = 16
+        time_resolution = 1. / 16
+        time = [time_resolution * 2 ** j for j in xrange(n_timepoints)]
+
+    # do the community-detection business proper on each connected component
+    clustering_mat = np.zeros((len(G_nodes), len(time)), dtype='int')
+    for k in xrange(len(connected_components)):
+        H = connected_components[k]
+
+        H_adj_mat = calculus.compute_adjacency_matrix(H)
+        H_nodes = H.nodes()
+        H_node_labels_map = dict((j, H_nodes[j])
+                                 for j in xrange(len(H_nodes)))
+
+        if len(H_nodes) > 1:
+            # XXX all the bottleneck is here, computing the exponential
+            # of a very big matrix
+            if logspace:
+                flow = calculus.compute_CTMC_flow_in_logspace(
+                    H_adj_mat,
+                    time_resolution=time_resolution,
+                    n_timepoints=n_timepoints,
+                    )
+            else:
+                flow = calculus.compute_CTMC_flow(H_adj_mat, time)
+
+        for j, Z in zip(xrange(n_timepoints), flow):
+            print
+            print ('\t[+] Zooming connected component number %i at Markov time'
+                   ' %.3f') % (k, time[j])
+            print
+
+            # handle degenerate graph with only one node
+            if len(H_nodes) == 1:
+                clustering_mat[H_nodes, j] = clustering_mat[:, j].max() + 1
+                continue
+
+            # compute flow matrix
+            input_markov_flow_filename = filename + ('%i_component_%i_markov_'
+                                                     'flow') % (j, k)
+            flow_H = nx.DiGraph(Z)
+
+            # derive a directed grapth representing the computed flow dynamics
+            flow_H = nx.relabel_nodes(flow_H, H_node_labels_map)
+            H_nodes = flow_H.nodes()
+            nx.write_pajek(flow_H, input_markov_flow_filename + '.net')
+
+            # run the Map Equation algorithm on the flow graph
+            markov_flow_community_filename = input_markov_flow_filename
+            markov_flow_community_filename += '.clu'
+            seed = random.randint(0, 10000)
+            print commands.getoutput("infomap %s %s %s" % (
+                    seed,
+                    input_markov_flow_filename + '.net',
+                    n_attempts))
+
+            # parse output community structure
+            partitioning = parsers.parse_infomapclu(
+                markov_flow_community_filename)
+
+            # paint this connected component
+            color_count = clustering_mat[:, j].max() + 1
+            for nodes in partitioning:
+                clustering_mat[nodes, j] = color_count
+                color_count += 1
+
+    # we're done; generate plots now
+    if do_plots:
+        title = ("Markov Zooming: Optimal community structures for different "
+                 "timescales")
+
+        print "[+] Generating plots .."
+        display_markov_zoom(
+            G, clustering_mat, time, title=title)
+        print "[+] Done."
+
+    return clustering_mat
+
+
+def display_markov_zoom(G, clustering_matrix, time, title=None):
+    """Function to display results of do_markov_zoom(..)
+
+    Parameters
+    ----------
+    G: nx.Graph object
+        Graph under consideration
+
+    clustering_matrix: 2 x 2 np.ndarray of shape of ints (#nodes, #time points)
+        clustering matrix for the network; each column contains the cluster
+        membership of nodes to modules, in at each markov time gap (resolution)
+
+    time: np.array
+        Markov time gaps/resolutions at which zooming has been done
+
+    title: string (optional, default None)
+        supertitle for all generated plots/subplots
+
+    """
+
+    n, m = clustering_matrix.shape
+    G_nodes = G.nodes()
+
+    Q_list = []
+    N_list = []
+    n_cols = 4
+    pos = nx.spring_layout(G)
+    n_rows = (len(time) - 1) / n_cols
+    n_rows += 1  # because subplots are counter from 1, not 0
+    n_rows += 1  # becase we'll add a modularity plot at the end
+
+    fig = plt.figure()
+    title = title if not title is None else (
+        "Markov Zooming: Optimal community structures for "
+        "different timescales")
+
+    fig.suptitle(title, fontsize=14)
+    for j in xrange(m):
+        modules = list(set(clustering_matrix[:, j]))
+        modules = dict((c, np.nonzero(clustering_matrix[:, j] == c)[0])
+                       for c in modules)
+
+        n_modules = len(modules)
+
+        # compute modularity
+        Q = calculus.compute_modularity(G, modules)
+        Q_list.append(Q)
+        N_list.append(len(set(clustering_matrix[:, j])))
+
+        # generate color for nodes in each module
+        node_color = np.zeros(len(G_nodes))
+        for color, c in zip(xrange(n_modules), modules):
+            node_color[clustering_matrix[:, j] == c] = color
+
+        # display colored graph proper
+        ax = plt.subplot2grid((n_rows, n_cols),
+                              np.unravel_index(j, (n_rows, n_cols)))
+        nx.draw(G, pos, node_color=node_color,
+                with_labels=False,
+                node_size=30,
+                )
+
+        ax.axis('off')
+        subplot_title = 'Markov time: %.3f\nCommunities: %i' % (
+            time[j], n_modules)
+        subplot_title += '\nModularity: %.3f' % Q
+        ax.set_title(subplot_title, fontsize=10)
+
+    # plot modularity curve over timescales
+    ax = plt.subplot2grid((n_rows, n_cols), (n_rows - 1, 0),
+                          colspan=n_cols / 2,
+                          rowspan=2)
+    # Q_list = np.array(Q_list)
+    # best_j = np.argmax(Q_list)
+    # best_Q = Q_list[best_j]
+    # best_j = np.nonzero(Q_list == best_Q)[0]
+    ax.plot(np.log2(time), Q_list)
+    # ax.plot(np.log2(time)[best_j],
+    #         Q_list[best_j],
+    #         marker='*', color='r')  # XXX ??!
+    ax.set_title("Modularity curve", fontsize=12)
+    ax.set_ylabel("modularity", fontsize=10)
+    ax.set_xlabel("time span (logspace)", fontsize=10)
+
+    # plot number of modules over timescales
+    ax = plt.subplot2grid((n_rows, n_cols), (n_rows - 1, n_cols / 2),
+                          colspan=n_cols / 2,
+                          rowspan=2)
+    ax.plot(np.log2(time), N_list)
+    ax.set_title("Community count in partition", fontsize=12)
+    ax.set_ylabel("# communities", fontsize=10)
+    ax.set_xlabel("time span (logspace)", fontsize=10)
+
+    # show plots
+    plt.tight_layout(pad=0.5, w_pad=0, h_pad=0)
+    plt.show()
+
+if __name__ == '__main__':
+    # sanitize cmd-line
+    if len(sys.argv) < 2:
+        print ("\r\n\tUsage: python %s <path_to_network_pajek_or_graphml_file>"
+               ) % sys.argv[0]
+        print ("\tExample: python %s "
+               "example_data/univ_dataset_TSPE.graphml\r\n" % sys.argv[0])
+        sys.exit(1)
+
+    print "[+] Reading network %s .." % sys.argv[1]
+
+    if sys.argv[1].endswith(".net") or sys.argv[1].endswith(".paj")\
+            or sys.argv[1].endswith(".NET"):
+        G = nx.Graph(nx.Graph(nx.read_pajek(sys.argv[1])))
+    elif sys.argv[1].endswith(".graphml"):
+        G = nx.Graph(nx.Graph(nx.read_graphml(sys.argv[1])))
+    elif sys.argv[1].endswith(".gml"):
+        G = nx.Graph(nx.Graph(nx.read_gml(sys.argv[1])))
+
+    print "[+] Done (read %i nodes and  %i edges)." % (len(G.nodes()),
+                                                       len(G.edges()))
+
+    # real business here
+    do_markov_zoom(G, filename=sys.argv[1],
+                   )
